db093b9fe2cdbf8cc0d51266e76d44ac21f6b3a3
Leo <losoviz@gmail.com>
2020-04-13 19:08:51 +0800

    Added declare(strict_types=1);

--- src/FieldResolvers/PostFieldResolver.php
+++ src/FieldResolvers/PostFieldResolver.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 namespace PoP\BlockMetadataWP\FieldResolvers;
 use Leoloso\BlockMetadata\Data;
 use Leoloso\BlockMetadata\Metadata;
--- src/FieldResolvers/TryNewFeaturesPostFieldResolver.php
+++ src/FieldResolvers/TryNewFeaturesPostFieldResolver.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 namespace PoP\BlockMetadataWP\FieldResolvers;
 
 use PoP\ComponentModel\Schema\SchemaDefinition;
