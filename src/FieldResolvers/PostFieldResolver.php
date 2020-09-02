2bab0460fee1adb78ed069ddf5abd132bbe9afad
Leonardo Losoviz <leo@getpop.org>
2020-09-02 12:42:39 +0800

    Added object type to $resultItem in `resolveValue`

--- src/FieldResolvers/PostFieldResolver.php
+++ src/FieldResolvers/PostFieldResolver.php
@@ -93,8 +93,22 @@ class PostFieldResolver extends AbstractDBDataFieldResolver
         return $schemaFieldArgs;
     }
 
-    public function resolveValue(TypeResolverInterface $typeResolver, $resultItem, string $fieldName, array $fieldArgs = [], ?array $variables = null, ?array $expressions = null, array $options = [])
-    {
+    /**
+     * @param array<string, mixed> $fieldArgs
+     * @param array<string, mixed>|null $variables
+     * @param array<string, mixed>|null $expressions
+     * @param array<string, mixed> $options
+     * @return mixed
+     */
+    public function resolveValue(
+        TypeResolverInterface $typeResolver,
+        object $resultItem,
+        string $fieldName,
+        array $fieldArgs = [],
+        ?array $variables = null,
+        ?array $expressions = null,
+        array $options = []
+    ) {
         $post = $resultItem;
         switch ($fieldName) {
             case 'blockMetadata':
