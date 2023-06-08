10c9ea9bdbf4f70a72b3f58b7cca7078dcc16241
leoloso <leo@getpop.org>
2023-06-08 08:28:30 +0000

    fixed cs (Renamed to disable the test)

--- tests/ModuleTestDisabled.php
+++ tests/ModuleTestDisabled.php
@@ -7,8 +7,8 @@ namespace PoPWPSchema\BlockMetadataWP;
 use PoP\ComponentModel\AbstractModuleTestCase;
 
 /**
- * Made abstract (and renamed) to disable the test
+ * Renamed to disable the test
  */
-abstract class ModuleTest_ extends AbstractModuleTestCase
+class ModuleTestDisabled extends AbstractModuleTestCase
 {
 }
